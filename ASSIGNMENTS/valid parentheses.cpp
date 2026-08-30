90be79ab0a9f416612c2fe89aeacdd23991a98dc
tejaswinimetta40-lgtm <tejaswinimetta40@gmail.com>
2026-08-30 13:09:57 +0530

    Create valid parentheses.cpp

---
+++ ASSIGNMENTS/valid parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for (char c : s) {
+            
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+                if (st.empty()) return false;
+                char top = st.top();
+                st.pop();
+                
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        // Return true if all opening brackets were closed
+        return st.empty();
+    }
+};
